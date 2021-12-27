2fd1738d0de18e088d61294794ebe3c55188bb17
Durgesh Pushpakar <durgeshpushpakar001@gmail.com>
2021-12-27 16:01:35 +0530

    #35 part completed added infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
 // api link
 // https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=89aff8ae38284d13aad285f81ab111d7
 export default class News extends Component {
@@ -21,8 +22,9 @@ export default class News extends Component {
         super(props);
         this.state={
             articles:[],
-            loading:false,
-            page:1
+            loading:true,
+            page:1,
+            totalResults:0
         }
         document.title=`${this.capitalize(this.props.category)} - NewsMonkey`;
     }
@@ -37,39 +39,45 @@ export default class News extends Component {
             totalResults:parsedData.totalResults
         }) 
     }
-    handleNext= async ()=>{
-        this.setState({page:this.state.page+1});
-        this.updateNews();
-        window.scroll(0,0);
-    }
-    handlePrev= async ()=>{     
-        this.setState({page:this.state.page-1});
-        this.updateNews();
-        window.scroll(0,0);
-    }
     async componentDidMount(){
         this.updateNews();
     }
-
+    fetchMoreData=async ()=>{
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=89aff8ae38284d13aad285f81ab111d7&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
+        let data=await fetch(url);
+        let parsedData=await data.json();
+        this.setState({
+            page:this.state.page+1,
+            articles:this.state.articles.concat(parsedData.articles),
+            totalResults:parsedData.totalResults,
+            loading:false
+        }) 
+    }
     
     render() {
         return (
-            <div className="container my-3">
+            <>
                 <h1 className="text-center">NewsMonkey- Top {this.capitalize(this.props.category)} Headlines </h1>
+                <h3>{this.state.totalResults} Results</h3>
                 {this.state.loading && <Spinner/>}
-                <h3>Page-{this.state.page}/{Math.ceil(this.state.totalResults/this.props.pageSize)}</h3>
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((e)=>{
-                        return <div className="col-md-4" key={e.url}>
-                            <NewsItem title={e.title} description={e.description} imageUrl={e.urlToImage} newsUrl={e.url} author={e.author} date={e.publishedAt} source={e.source.name}/>
-                        </div>
-                    })}
-                </div>
-                <div className="container my-3 d-flex justify-content-between">
-                    <button disabled={this.state.page<=1} type="button" className="btn btn-dark mx-3" onClick={this.handlePrev}>&larr; Previous</button>
-                    <button disabled={!(Math.ceil(this.state.totalResults/this.props.pageSize)>=this.state.page+1)} type="button" className="btn btn-dark mx-3" onClick={this.handleNext}>Next &rarr;</button>
-                </div>
-            </div>
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length!=this.state.totalResults}
+                    loader={<Spinner/>}
+                >
+                    <div className="container">
+                    <div className="row">
+                        {this.state.articles.map((e)=>{
+                            return <div className="col-md-4" key={e.url}>
+                                <NewsItem title={e.title} description={e.description} imageUrl={e.urlToImage} newsUrl={e.url} author={e.author} date={e.publishedAt} source={e.source.name}/>
+                            </div>
+                        })}
+                    </div>
+                    </div>
+                </InfiniteScroll>
+            </>
         )        
     }
 }
